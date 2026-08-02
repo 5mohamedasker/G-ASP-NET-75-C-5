8f06e6a54d88c40863f93b5b20b34799627ca065
5mohamedasker <5mohamedasker@gmail.com>
2026-08-02 12:44:00 +0300

    Question01

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,9 +1,25 @@
 ﻿namespace ConsoleApp5
 {
+    internal class Book
+    {
+        private string password = "secret";
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region Question01
+            ////1.Add a private string password = "secret"; field to a Book class.
+            ////Try to print it from Main (outside the class).
+            ////What happens, and why?
+            //Book book = new Book();
+            //Console.WriteLine(book.password);
+            ///*What happens, And why
+            // Error
+            // because I don't have permission to use the password
+            // outside of the class.
+            //*/
+            #endregion
         }
     }
 }
